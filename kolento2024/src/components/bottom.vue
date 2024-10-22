9940d4073e31972977a2f129ca389a66aa107dae
Kolento <515835543@qq.com>
2024-10-22 23:58:14 +0800

    修改
    Signed-off-by: Kolento <515835543@qq.com>

---
+++ kolento2024/src/components/bottom.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="bottom">
+        <span>QQ咨询：515835543 </span>
+        <span>备案号：xxxxxxxx</span>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+    .bottom {
+        margin-top: 1rem;
+        padding: 0 0.2rem 0 1rem;
+        width: 100%;
+        height: 3.5rem;
+        background: #df8888;
+        display: flex;
+        align-items: center;
+        color:#fff;
+        justify-content: center;
+        span {margin:0 1rem;}
+    }
+</style>
